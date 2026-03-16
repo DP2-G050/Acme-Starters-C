e4aad9208cadf2e48fa08118150d4afd9cd78436
Rafa Segura <rafseggom@alum.us.es>
2026-03-16 21:56:18 +0100

    crud student2

---
+++ src/main/java/acme/features/spokesperson/milestone/SpokespersonMilestoneUpdateService.java
@@ -0,0 +1,67 @@
+
+package acme.features.spokesperson.milestone;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.services.AbstractService;
+import acme.entities.campaigns.Campaign;
+import acme.entities.campaigns.Milestone;
+import acme.realms.Spokesperson;
+
+@Service
+public class SpokespersonMilestoneUpdateService extends AbstractService<Spokesperson, Milestone> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private SpokespersonMilestoneRepository	repository;
+
+	private Milestone						milestone;
+
+	// AbstractService interface -------------------------------------------
+
+
+	@Override
+	public void load() {
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		this.milestone = this.repository.findMilestoneById(id);
+	}
+
+	@Override
+	public void authorise() {
+		boolean status;
+		int campaignId;
+		Campaign campaign;
+
+		campaignId = this.milestone.getCampaign().getId();
+		campaign = this.repository.findCampaignById(campaignId);
+
+		status = campaign != null && //
+			campaign.isDraftMode() && campaign.getSpokesperson().isPrincipal();
+		super.setAuthorised(status);
+	}
+
+	@Override
+	public void bind() {
+		super.bindObject(this.milestone, "title", "achievements", "effort", "kind");
+	}
+
+	@Override
+	public void validate() {
+		super.validateObject(this.milestone);
+	}
+
+	@Override
+	public void execute() {
+		this.repository.save(this.milestone);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObject(this.milestone, "title", "achievements", "effort", "kind");
+	}
+
+}
