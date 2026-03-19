0d12034f06d0064bf2d63ebf58c49bfb1e175f11
Nerea <nerjimado@alum.us.es>
2026-03-19 13:00:01 +0100

    Arreglado form

--- src/main/java/acme/features/inventor/part/InventorPartUpdateService.java
+++ src/main/java/acme/features/inventor/part/InventorPartUpdateService.java
@@ -4,9 +4,12 @@ package acme.features.inventor.part;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import acme.client.components.models.Tuple;
+import acme.client.components.views.SelectChoices;
 import acme.client.services.AbstractService;
 import acme.entities.invention.Invention;
 import acme.entities.invention.Part;
+import acme.entities.invention.PartKind;
 import acme.realms.Inventor;
 
 @Service
@@ -68,7 +71,15 @@ public class InventorPartUpdateService extends AbstractService<Inventor, Part> {
 
 	@Override
 	public void unbind() {
-		super.unbindObject(this.part, "name", "description", "cost", "kind");
+		SelectChoices choices;
+		Tuple tuple;
+
+		choices = SelectChoices.from(PartKind.class, this.part.getKind());
+
+		tuple = super.unbindObject(this.part, "name", "description", "cost", "kind");
+		tuple.put("inventionId", this.part.getInvention().getId());
+		tuple.put("draftMode", this.part.getInvention().isDraftMode());
+		tuple.put("kinds", choices);
 	}
 
 }
